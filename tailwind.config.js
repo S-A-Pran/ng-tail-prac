ab598c140e12f7e1ef5087654ae1fc2969c09143
efg-group <efggroup4@gmail.com>
2022-02-13 17:50:49 +0600

    project part mb

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.{html,ts}"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
