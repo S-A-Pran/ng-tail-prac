59978482472ffe1d057373dc555cf52fc17cd761
efg-group <efggroup4@gmail.com>
2022-02-19 17:26:13 +0600

    added footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,38 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
+    screens: {
+      wide: { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
+
+      desktop: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      laptop: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      tablet: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      mobile: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+      mini: { max: "480px" },
+      // => @media (max-width: 639px) { ... }
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   variants: {
     extend: {},
